ad0b845194068694b704526a181b631ccbedd9b8
Pcoutros <118582425+Pcoutros@users.noreply.github.com>
2024-08-14 21:31:41 -0500

    Update UnsortedException.java

--- src/UnsortedException.java
+++ src/UnsortedException.java
@@ -1,8 +1,6 @@
 /**
  * Pete Coutros
- * CMSC451
  * June 8, 2024
- * Project 1 - Benchmarking Bubble and Insertion Sort
  * 
  * This class is a custom exception class that is a child of Exception. It is used to denote an unsorted array
  */
