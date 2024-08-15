6b4feda88e3aa50aa8f6121c1289d176a401eac5
Pcoutros <118582425+Pcoutros@users.noreply.github.com>
2024-08-14 21:23:36 -0500

    Update BubbleSort.java

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -24,40 +24,40 @@ public class BubbleSort extends AbstractSort {
 	 * @return int[] arr
 	 * @throws UnsortedException 
 	 */
-    public int[] sort(int arr[]) throws UnsortedException {
+    	public int[] sort(int arr[]) throws UnsortedException {
     	
-    	//Begin timer and initialize counter to 0
-    	startSort();
-    	
-        int i, j, temp;
-        int n = arr.length;
-        boolean swapped;
-        for (i = 0; i < n - 1; i++) {
-            swapped = false;
-            for (j = 0; j < n - i - 1; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    
-                    // Swap arr[j] and arr[j+1]
-                    temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                    swapped = true;
-                    
-                    //The swap is the critical operation
-                    incrementCount();
-                }
-            }
-
-            // If no two elements were swapped by inner loop, then break
-            if (swapped == false)
-                break;
-        }
-        
-        //Record elapsed time
-        endSort();
-        
-        //Check to see if array is sorted
-        verifySort(arr);        
-        return arr;
-    }
+	    	//Begin timer and initialize counter to 0
+	    	startSort();
+	    	
+	        int i, j, temp;
+	        int n = arr.length;
+	        boolean swapped;
+	        for (i = 0; i < n - 1; i++) {
+	            swapped = false;
+	            for (j = 0; j < n - i - 1; j++) {
+	                if (arr[j] > arr[j + 1]) {
+	                    
+	                    // Swap arr[j] and arr[j+1]
+	                    temp = arr[j];
+	                    arr[j] = arr[j + 1];
+	                    arr[j + 1] = temp;
+	                    swapped = true;
+	                    
+	                    //The swap is the critical operation
+	                    incrementCount();
+	                }
+	            }
+	
+	            // If no two elements were swapped by inner loop, then break
+	            if (swapped == false)
+	                break;
+	        }
+	        
+	        //Record elapsed time
+	        endSort();
+	        
+	        //Check to see if array is sorted
+	        verifySort(arr);        
+	        return arr;
+	}
 }
