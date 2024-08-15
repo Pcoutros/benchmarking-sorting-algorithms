b8a5c5f17b083aeaf5bc0a8850d8798b99fb3017
Pcoutros <118582425+Pcoutros@users.noreply.github.com>
2024-08-14 21:24:37 -0500

    Update InsertionSort.java

--- src/InsertionSort.java
+++ src/InsertionSort.java
@@ -23,36 +23,36 @@ public class InsertionSort extends AbstractSort {
 	 * @return int[] arr
 	 * @throws UnsortedException 
 	 */
-    public int[] sort(int arr[]) throws UnsortedException {
-    	
-    	//Begin timer and initialize counter to 0
-    	startSort();
-    	
-        int n = arr.length;
-        for (int i = 1; i < n; ++i) {
-            int key = arr[i];
-            int j = i - 1;
-
-            /* Move elements of arr[0..i-1], that are
-               greater than key, to one position ahead
-               of their current position */
-            while (j >= 0 && arr[j] > key) {
-            	
-                arr[j + 1] = arr[j];
-                j = j - 1;
-                
-            	//The swap is the critical operation of the function
-            	incrementCount();
-            }
-            arr[j + 1] = key;
-        }
-        
-        //Record elapsed time
-        endSort();
-        
-        //Check to see if array is sorted
-        verifySort(arr);
-        
-        return arr;
-    }
+	public int[] sort(int arr[]) throws UnsortedException {
+	    	
+		//Begin timer and initialize counter to 0
+	    	startSort();
+	    	
+	        int n = arr.length;
+	        for (int i = 1; i < n; ++i) {
+	            int key = arr[i];
+	            int j = i - 1;
+	
+	            /* Move elements of arr[0..i-1], that are
+	               greater than key, to one position ahead
+	               of their current position */
+	            while (j >= 0 && arr[j] > key) {
+	            	
+	                arr[j + 1] = arr[j];
+	                j = j - 1;
+	                
+	            	//The swap is the critical operation of the function
+	            	incrementCount();
+	            }
+	            arr[j + 1] = key;
+	        }
+	        
+	        //Record elapsed time
+	        endSort();
+	        
+	        //Check to see if array is sorted
+	        verifySort(arr);
+	        
+	        return arr;
+	}
 }
